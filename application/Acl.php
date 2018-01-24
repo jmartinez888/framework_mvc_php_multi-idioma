b30a315c7bd43d429d30acb3104ad3cca60135a8
jmartinez888 <34007650+jmartinez888@users.noreply.github.com>
2018-01-24 11:32:58 -0500

    ACL - Permisos y Roles a nivel de modulo
    Se modificó la funcion  getRolesUsuario, getPermisosRoles, getPermisosRolesIds para trabajar con permisos a nivel de modulos y con varios roles

--- application/Acl.php
+++ application/Acl.php
@@ -4,32 +4,32 @@ class ACL
 {
     private $_registry;
     private $_db;
-    private $_id;
-    private $_role;
+    private $_Usu_IdUsuario;
+    private $_roles;
     private $_permisos;
     
-    public function __construct($id = false)
+    public function __construct($idUsuario = false)
     {
-        if($id)
+        if($idUsuario)
         {
-            $this->_id = (int) $id;
+            $this->_Usu_IdUsuario = (int) $idUsuario;
         }
         else
         {
             if(Session::get('id_usuario'))
             {
-                $this->_id = Session::get('id_usuario');
+                $this->_Usu_IdUsuario = Session::get('id_usuario');
             }
             else
             {
-                $this->_id = 0;
+                $this->_Usu_IdUsuario = 0;
             }
         }
                 
         $this->_registry = Registry::getInstancia();
         $this->_db = $this->_registry->_db;
-        $this->_role = $this->getRole();
-        $this->_permisos = $this->getPermisosRole();
+        $this->_roles = $this->getRolesUsuario();
+        $this->_permisos = $this->getPermisosRoles();
         $this->compilarAcl();
     }
     
@@ -38,116 +38,100 @@ class ACL
         $this->_permisos = array_merge($this->_permisos, $this->getPermisosUsuario());
     }
     
-    public function getRole()
+    public function getRolesUsuario()
     { 
-        $role = $this->_db->query("select Rol_IdRol from usuario" .
-                                " where Usu_IdUsuario = {$this->_id}");       
-                
-        $role = $role->fetch();
-        if($role['Rol_IdRol'])
+        try{
+            $sql = "call s_s_listar_roles_x_id_usuario(?)";
+            $result = $this->_db->prepare($sql);
+            $result->bindParam(1, $this->_Usu_IdUsuario, PDO::PARAM_INT);
+            $result->execute();
+            $roles = $result->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $exception) {
+            // $this->registrarBitacora("acl(indexModel)", "getPermisos", "Error Model", $exception);
+            $exception->getTraceAsString();
+        }
+
+
+        if(count($roles) > 0)
         {          
-            return $role['Rol_IdRol'];
+            return $roles;
         }
         else 
-            return 0;
-                
+            return 0;                
     }
     
-    public function getPermisosRoleId()
-    {
-        $ids = $this->_db->query("select Per_IdPermiso from permisos_rol " .
-                "where Rol_IdRol = {$this->_role}");
-                
-        $ids = $ids->fetchAll(PDO::FETCH_ASSOC);
-        
-        $id = array();
-        
-        for($i = 0; $i < count($ids); $i++){
-            $id[] = $ids[$i]['Per_IdPermiso'];
-        }
-        
-        return $id;
-    }
-    
-    public function getPermisosRole()
-    {     
-        $permisos = $this->_db->query("select * from permisos_rol where Rol_IdRol = {$this->_role}"
-                );   
-        $permisos = $permisos->fetchAll(PDO::FETCH_ASSOC);
-          
+    public function getPermisosRoles()
+    {    
         $data = array();
-        
-        for($i = 0; $i < count($permisos); $i++)
-        {
-            $key = $this->getPermisoKey($permisos[$i]['Per_IdPermiso']);
-            if($key == ''){continue;}
-            
-            if($permisos[$i]['Rol_Valor'] == 1)
+        $RolesArray = $this->_roles;
+
+        if($RolesArray && count($RolesArray) > 0){
+            for($i = 0; $i < count($RolesArray); $i++)
             {
-                $v = true;
+                try{
+                    $sql = "call s_s_listar_permisos_rol_x_id_rol(?)";
+                    $result = $this->_db->prepare($sql);
+                    $result->bindParam(1, $RolesArray[$i]['Rol_IdRol'], PDO::PARAM_INT);
+                    $result->execute();
+                    $permisosArray = $result->fetchAll(PDO::FETCH_ASSOC);
+                } catch (PDOException $exception) {
+                    // $this->registrarBitacora("acl(indexModel)", "getPermisos", "Error Model", $exception);
+                    $exception->getTraceAsString();
+                }
+
+
+                for($j = 0; $j < count($permisosArray); $j++)
+                {
+                    $key = $this->getPermisoKey($permisosArray[$j]['Per_IdPermiso']);
+                    if($key == ''){continue;}
+                    
+                    if($permisosArray[$j]['Per_Valor'] == 1)
+                    {
+                        $v = true;
+                    }
+                    else
+                    {
+                        $v = false; 
+                    }
+                    
+                    $data[$key] = array(
+                        'key' => $key,
+                        'permiso' => $this->getPermisoNombre($permisosArray[$j]['Per_IdPermiso']),
+                        'rol' => $RolesArray[$i]['Rol_Nombre'],
+                        'valor' => $v,
+                        'heredado' => true,
+                        'id' => $permisosArray[$j]['Per_IdPermiso']
+                    );
+                }
             }
-            else
-            {
-                $v = false;
-            }
-            
-            $data[$key] = array(
-                'key' => $key,
-                'permiso' => $this->getPermisoNombre($permisos[$i]['Per_IdPermiso']),
-                'valor' => $v,
-                'heredado' => true,
-                'id' => $permisos[$i]['Per_IdPermiso']
-            );
         }
-        
         return $data;
+
     }
-    
-    public function getPermisoKey($permisoID)
-    {
-        $permisoID = (int) $permisoID;
-        
-        $key = $this->_db->query(
-                "select Per_Ckey as `key` from permisos " .
-                "where Per_IdPermiso = {$permisoID}"
-                );
-                
-        $key = $key->fetch();
-        return $key['key'];
-    }
-    
-    public function getPermisoNombre($permisoID)
-    {
-        $permisoID = (int) $permisoID;
-        
-        $key = $this->_db->query(
-                "select Per_Permiso from permisos " .
-                "where Per_IdPermiso = {$permisoID}"
-                );
-                
-        $key = $key->fetch();
-        return $key['Per_Permiso'];
-    }
-    
+
     public function getPermisosUsuario()
     {
-        $ids = $this->getPermisosRoleId();
-        
-        if(count($ids))
+        $idPermisos = $this->getPermisosRolesIds();
+        if($idPermisos && count($idPermisos) > 0)
         {
-            $permisos = $this->_db->query(
-                    "select * from permisos_usuario " .
-                    "where Usu_IdUsuario = {$this->_id} " .
-                    "and Per_IdPermiso in (" . implode(",", $ids) . ")"
-                    );
+            try{
+                $sql = "call s_s_listar_permisos_usuario_x_id_usuario(?,?)";
+                $result = $this->_db->prepare($sql);
+                $result->bindParam(1, $this->_Usu_IdUsuario, PDO::PARAM_INT);
+                $result->bindParam(2, implode(",", $idPermisos), PDO::PARAM_STR);
+                $result->execute();
+                $permisos = $result->fetchAll(PDO::FETCH_ASSOC);
+            } catch (PDOException $exception) {
+                // $this->registrarBitacora("acl(indexModel)", "getPermisos", "Error Model", $exception);
+                $exception->getTraceAsString();
+            }
 
-            $permisos = $permisos->fetchAll(PDO::FETCH_ASSOC);
         }
         else
         {
             $permisos = array();
         }
-        
+
         $data = array();
         
         for($i = 0; $i < count($permisos); $i++)
@@ -158,7 +142,7 @@ class ACL
                 continue;
             }
             
-            if($permisos[$i]['Usu_Valor'] == 1)
+            if($permisos[$i]['Peu_Valor'] == 1)
             {
                 $v = true;
             }
@@ -170,6 +154,7 @@ class ACL
             $data[$key] = array(
                 'key' => $key,
                 'permiso' => $this->getPermisoNombre($permisos[$i]['Per_IdPermiso']),
+                'rol' => " - ",
                 'valor' => $v,
                 'heredado' => false,
                 'id' => $permisos[$i]['Per_IdPermiso']
@@ -178,6 +163,89 @@ class ACL
         
         return $data;
     }
+
+    public function getPermisosRolesIds()
+    {
+        $idPermisosArray = array();
+        $RolesArray = $this->_roles;
+
+        if($RolesArray && count($RolesArray) > 0){
+            for($i = 0; $i < count($RolesArray); $i++)
+            {                
+                try{
+                    $sql = "call s_s_listar_ids_permisos_x_id_rol(?)";
+                    $result = $this->_db->prepare($sql);
+                    $result->bindParam(1, $RolesArray[$i]['Rol_IdRol'], PDO::PARAM_INT);
+                    $result->execute();
+                    $idPermisos = $result->fetchAll(PDO::FETCH_ASSOC);
+                } catch (PDOException $exception) {
+                    // $this->registrarBitacora("acl(indexModel)", "getPermisos", "Error Model", $exception);
+                    $exception->getTraceAsString();
+                }
+                
+                for($j = 0; $j < count($idPermisos); $j++){
+                    $idPermisosArray[] = $idPermisos[$j]['Per_IdPermiso'];
+                }
+            }
+        }
+        return $idPermisosArray;
+    }   
+    
+    
+    public function getPermisoKey($permisoID)
+    {
+        $permisoID = (int) $permisoID;
+        
+        try{
+                $sql = "call s_s_obtener_ckey_permiso_x_id(?)";
+                $result = $this->_db->prepare($sql);
+                $result->bindParam(1, $permisoID, PDO::PARAM_INT);
+                $result->execute();
+                $key = $result->fetch(PDO::FETCH_ASSOC);
+            } catch (PDOException $exception) {
+                // $this->registrarBitacora("acl(indexModel)", "getPermisos", "Error Model", $exception);
+                $exception->getTraceAsString();
+            }
+
+        return $key['key'];
+    }
+    
+    public function getPermisoNombre($permisoID)
+    {
+        $permisoID = (int) $permisoID;
+        
+        try{
+                $sql = "call s_s_obtener_nombre_permiso_x_id(?)";
+                $result = $this->_db->prepare($sql);
+                $result->bindParam(1, $permisoID, PDO::PARAM_INT);
+                $result->execute();
+                $_Per_Nombre = $result->fetch(PDO::FETCH_ASSOC);
+            } catch (PDOException $exception) {
+                // $this->registrarBitacora("acl(indexModel)", "getPermisos", "Error Model", $exception);
+                $exception->getTraceAsString();
+            }
+
+        return $_Per_Nombre['Per_Nombre'];
+    }
+
+    public function getRolNombre($rolID)
+    {
+        $rolID = (int) $rolID;
+        try{
+                $sql = "call s_s_obtener_nombre_rol_x_id(?)";
+                $result = $this->_db->prepare($sql);
+                $result->bindParam(1, $permisoID, PDO::PARAM_INT);
+                $result->execute();
+                $_Rol_Nombre = $result->fetch(PDO::FETCH_ASSOC);
+            } catch (PDOException $exception) {
+                // $this->registrarBitacora("acl(indexModel)", "getPermisos", "Error Model", $exception);
+                $exception->getTraceAsString();
+            }
+
+        return $_Rol_Nombre['Rol_Nombre'];
+    }
+    
+    
     
     public function getPermisos()
     {
@@ -187,6 +255,7 @@ class ACL
     
     public function permiso($key)
     {
+
         if(array_key_exists($key, $this->_permisos)){
             if($this->_permisos[$key]['valor'] == true || $this->_permisos[$key]['valor'] == 1)
             {
@@ -221,7 +290,6 @@ class ACL
         }else{
             header("location:" . BASE_URL . "usuarios/login/index/$url");
         }
-        
         exit;
     }
     public function autenticado()
